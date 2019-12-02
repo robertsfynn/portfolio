447782b16334e34e3bf0d28f6e3de4eb8f54180c
Fynn Roberts <robertsfynn@gmail.com>
2019-12-02 21:59:20 +0100

    removed margins for testing animations

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,19 +1,23 @@
-import React, { useState } from "react"
+import React from "react"
 import { Row, Col } from "react-grid-system"
 import mobileImage from "../../images/mobile.png"
 import macImage from "../../images/imac.png"
 import PortfolioItem from "./PortfolioItem"
+import { Header2 } from "../Text/Text"
 
 const Portfolio = () => {
   return (
-    <Row style={{ marginTop: 1000 }}>
-      <Col xs={12} sm={12} md={5}>
-        <PortfolioItem image={mobileImage} item={1} />
-      </Col>
-      <Col xs={12} sm={12} md={7}>
-        <PortfolioItem image={macImage} item={2} />
-      </Col>
-    </Row>
+    <div style={{ marginTop: 100 }}>
+      <Header2>Portfolio</Header2>
+      <Row>
+        <Col xs={12} md={5}>
+          <PortfolioItem image={mobileImage} item={1} />
+        </Col>
+        <Col xs={12} md={7}>
+          <PortfolioItem image={macImage} item={2} />
+        </Col>
+      </Row>
+    </div>
   )
 }
 
