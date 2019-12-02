63a1ccef3614d48ee07932adec1566bb16673820
Fynn Roberts <robertsfynn@gmail.com>
2019-12-02 21:54:23 +0100

    mobile test

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,10 +7,10 @@ import PortfolioItem from "./PortfolioItem"
 const Portfolio = () => {
   return (
     <Row style={{ marginTop: 1000 }}>
-      <Col xs={12} md={5}>
+      <Col xs={12} sm={12} md={5}>
         <PortfolioItem image={mobileImage} item={1} />
       </Col>
-      <Col xs={12} md={7}>
+      <Col xs={12} sm={12} md={7}>
         <PortfolioItem image={macImage} item={2} />
       </Col>
     </Row>
