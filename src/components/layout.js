b5d9e3698d0976ff52b672a315e276f351906582
Fynn Roberts <robertsfynn@gmail.com>
2019-12-08 17:35:24 +0100

    added breakpoints so 1024px is always biggest

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,8 +59,8 @@ const Layout = ({ children }) => {
       xs: 1,
       sm: 48, // 768px
       md: 64, // 1024px
-      lg: 90, // 1440px
-      xl: 120, // 1920px
+      lg: 64, // 1440px
+      xl: 64, // 1920px
     },
   }
 
