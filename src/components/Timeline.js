01a0bcdd17bd1e97e8c76140e43b92ff91e01470
Fynn Roberts <robertsfynn@gmail.com>
2019-12-11 14:06:10 +0100

    added ugly way to make the timeline items go left right left

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,15 +2,29 @@ import React from "react"
 import { Container } from "react-awesome-styled-grid"
 import { Line } from "./Animations"
 import { Section } from "./Typograhpy"
+import styled from "styled-components"
+
+const StyledTimeline = styled.ul`
+  > :nth-child(2n) li div:last-child {
+    right: 45px;
+    left: initial;
+  }
+`
 
 const Timeline = () => {
   return (
     <Section id="timeline">
       <Container>
-        <ul>
+        <StyledTimeline>
           <Line threshold={1}>You can do it</Line>
           <Line threshold={1}>You can do it</Line>
-        </ul>
+          <Line threshold={1}>You can do it</Line>
+          <Line threshold={1}>You can do it</Line>
+          <Line threshold={1}>You can do it</Line>
+          <Line threshold={1}>You can do it</Line>
+          <Line threshold={1}>You can do it</Line>
+          <Line threshold={1}>You can do it</Line>
+        </StyledTimeline>
       </Container>
     </Section>
   )
