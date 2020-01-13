24453dddfa453aca8778a50188c4552e30ecb043
Fynn Roberts <robertsfynn@gmail.com>
2020-01-13 17:30:48 +0100

    removed pwa again

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,6 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
   ],
 }
