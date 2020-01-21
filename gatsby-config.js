14429e5212354de320faba98ac831cebbd2f6fd8
Fynn Roberts <robertsfynn@gmail.com>
2020-01-21 13:39:00 +0100

    changed tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,22 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-138655358-1',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'robertsfynn.dev',
+        trackingId: 'UA-138655358-2',
       },
     },
   ],
